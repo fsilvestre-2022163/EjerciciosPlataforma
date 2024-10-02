fa26e3af0a147ae1c8bd98d89a227e9db54c68c2
fsilvestre-2022163 <fsilvestre-2022163@kinal.edu.gt>
2024-10-02 15:24:30 -0600

    se creo rama

--- src/main/java/com/fsilvestre/postgre/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fsilvestre.postgre.repository;
-
-import com.fsilvestre.postgre.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
